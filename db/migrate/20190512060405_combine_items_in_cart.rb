6011e904d2816fa8fe1e0e3523ffd948d0fc0055
obmax <Oberemko.maxim@gmail.com>
2019-05-12 14:32:33 +0300

    complete cart base logic

---
+++ db/migrate/20190512060405_combine_items_in_cart.rb
@@ -0,0 +1,17 @@
+class CombineItemsInCart < ActiveRecord::Migration[5.2]
+  def change
+    Cart.all.each do |cart|
+      sums = cart.line_items.group(:product_id).sum(:quantity)
+
+      sums.each do |product_id, quantity|
+        if quantity > 1
+          cart.line_items.where(product_id: product_id).delete_all
+
+          item = cart.line_items.build(product_id: product_id)
+          item.quantity = quantity
+          item.save!
+        end
+      end
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_11_214153) do
+ActiveRecord::Schema.define(version: 2019_05_12_060405) do
 
   create_table "carts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2019_05_11_214153) do
     t.bigint "cart_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "quantity", default: 1
     t.index ["cart_id"], name: "index_line_items_on_cart_id"
     t.index ["product_id"], name: "index_line_items_on_product_id"
   end
