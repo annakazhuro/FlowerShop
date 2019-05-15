b98f716a3d7518ffe4199525eb525b671a354dd5
obmax <Oberemko.maxim@gmail.com>
2019-05-16 01:22:59 +0300

    product bg image

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -33,6 +33,10 @@ class ImageUploader < CarrierWave::Uploader::Base
     process resize_to_fit: [50, 50]
   end
 
+  version :medium do
+    process resize_to_fit: [100, 100]
+  end
+
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_whitelist
